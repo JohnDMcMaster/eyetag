a48dbfb34dd62344fea77f919deefbcf8ad9e464
John McMaster <johndmcmaster@gmail.com>
2020-08-21 01:21:30 -0700

    decode SELECT
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- main.py
+++ main.py
@@ -178,13 +178,13 @@ def parse_dpap_tdi(tdi, prefix=""):
         rnw = "r"
     else:
         rnw = "w"
-    print("  %s tdi data 0x%08X,  a %s, rnw %s" % (prefix, data, a, rnw))
+    print("  %s tdi data 0x%08X, a %s, rnw %s" % (prefix, data, a, rnw))
     return data, a, rnw
 
 def prepare_format(format):
     pass
 
-ctrlstat = [
+ctrlstat_fmt = [
     # MSB
     'CSYSPWRUPACK',
     'CSYSPWRUPREQ',
@@ -204,6 +204,13 @@ ctrlstat = [
     'ORUNDETECT',
     # LSB
     ]
+
+select_fmt = [
+    ('APSEL', 8),
+    ('RES0', 16),
+    ('APBANKSEL', 4),
+    ('DPBANKSEL', 4),
+]
 
 def bits_decode(formats, nbits, val):
     def maskn(n):
@@ -228,7 +235,10 @@ def bits_decode(formats, nbits, val):
     return ret
 
 def decode_ctrlstat(reg):
-    return bits_decode(ctrlstat, 32, reg)
+    return bits_decode(ctrlstat_fmt, 32, reg)
+
+def decode_select(reg):
+    return bits_decode(select_fmt, 32, reg)
 
 def bits_str_sparse(vald):
     ret = []
@@ -247,6 +257,7 @@ class EyeTAG:
     def __init__(self):
         self.cmd_max = float('inf')
         # self.cmd_max = 8
+        self.jtag_verbose = 0
 
     def iter_jtag_wr(self, fn_in):
         self.ir = None
@@ -256,14 +267,14 @@ class EyeTAG:
         self.part_ver = None
         self.cmdn = 0
         for p in iter_decode(fn_in):
-            t, state, tdi, tdo = p
+            _t, state, tdi, tdo = p
             if state == 'Run-Test/Idle':
                 self.cmdn += 1
                 if self.cmdn >= self.cmd_max:
                     return
-                print("")
-                print("Group %u" % self.cmdn)
-            print(state)
+                self.jtag_verbose and print("")
+                self.jtag_verbose and print("Group %u" % self.cmdn)
+            self.jtag_verbose and print(state)
             if state == "Test-Logic-Reset":
                 ir = "IDCODE"
             elif state == "Capture-IR":
@@ -271,21 +282,22 @@ class EyeTAG:
             elif state == "Shift-IR":
                 ir_raw = tdi2ir(tdi)
                 ir = match_ir(ir_raw)
+                print("")
                 print("  New IR: %s (%s)" % (ir_raw, ir))
             elif state == "Shift-DR":
-                print("  TDI %u %s" % (len(tdi), tdi))
-                print("  TDO %u %s" %(len(tdo), tdo))
+                self.jtag_verbose and print("  TDI %u %s" % (len(tdi), tdi))
+                self.jtag_verbose and print("  TDO %u %s" %(len(tdo), tdo))
                 if ir == "IDCODE" and self.last_state == "Capture-DR":
-                    print("  IDCODE (full)", tdo[-72:])
+                    self.jtag_verbose and print("  IDCODE (full)", tdo[-72:])
                     id32 = tdo2dr(tdo)
-                    print("  IDCODE (32)", id32)
+                    self.jtag_verbose and print("  IDCODE (32)", id32)
                     vendor, part, part_ver = parse_idcode_str(id32)
                 elif ir == "DPACC":
                     # several things in bypass (1 bit each)
                     yield ir, tdi[2:], tdo[2:]
                 elif ir == "APACC":
                     yield ir, tdi[2:], tdo[2:]
-            last_state = state
+            self.last_state = state
 
     def next_decode(self, last_data, this_data):
         """
@@ -293,6 +305,12 @@ class EyeTAG:
         If possible, use the response in this_data
         Ignore the old response in last_data and the new command in this_data
         """
+
+        def fmt_u32_op(i):
+            if i is None:
+                return "none"
+            else:
+                return "0x%08X" % i
 
         # Command
         last_ir, last_tdi_dec, last_tdo_dec, last_cmdi = last_data
@@ -304,6 +322,7 @@ class EyeTAG:
             this_dir = None
             this_tdi_dec = None
             this_tdo_dec = None
+            this_cmdi = None
 
         if last_ir == "DPACC":
             datai, a, rnw = last_tdi_dec
@@ -315,18 +334,29 @@ class EyeTAG:
                 3: "RDBUFF",
                 }[a]
             if rnw == "r":
-                this_data, this_ack = this_tdo_dec
-                print("  DPACC R %s (cmd %u/%u): 0x%08X, ack %s" % (astr, last_cmdi, this_cmdi, this_data, this_ack))
-                if astr == "CTRL_STAT":
-                    print("  Flags: %s" % bits_str_sparse(decode_ctrlstat(this_data)))
+                if this_tdo_dec is None:
+                    this_data = None
+                    this_ack = None
+                else:
+                    this_data, this_ack = this_tdo_dec
+                print("  DPACC R %s (cmd %u/%s): %s, ack %s" % (astr, last_cmdi, this_cmdi, fmt_u32_op(this_data), this_ack))
+                if astr == "CTRL_STAT" and this_data is not None:
+                    print("    Flags: %s" % bits_str_sparse(decode_ctrlstat(this_data)))
+                if astr == "SELECT" and this_data is not None:
+                    print("    Flags: %s" % bits_str_sparse(decode_select(this_data)))
             else:
-                _this_data, this_ack = this_tdo_dec
-                print("  DPACC W %s (cmd %u/%u): 0x%08X, ack %s" % (astr, last_cmdi, this_cmdi, datai, this_ack))
+                if this_tdo_dec is None:
+                    this_ack = None
+                else:
+                    _this_data, this_ack = this_tdo_dec
+                print("  DPACC W %s (cmd %u/%s): 0x%08X, ack %s" % (astr, last_cmdi, this_cmdi, datai, this_ack))
                 if astr == "CTRL_STAT":
                     print("  Flags: %s" % bits_str_sparse(decode_ctrlstat(datai)))
+                if astr == "SELECT":
+                    print("    Flags: %s" % bits_str_sparse(decode_select(datai)))
 
         elif last_ir == "APACC":
-            pass
+            print("  APACC (cmd %u/%s)" % (last_cmdi, this_cmdi))
         else:
             assert 0, this_ir
 
@@ -334,18 +364,21 @@ class EyeTAG:
         ir, tdi, tdo = next(self.cmds)
         if ir == "DPACC":
             # Table 3-6 JTAG-DP target response summary, when previous scan a was a DPACC access
-            tdi_dec = parse_dpap_tdi(tdi, "DPACC")
-            tdo_dec = parse_dpap_tdo(tdo, "DPACC")
+            tdi_dec = parse_dpap_tdi(tdi, "cmd %u DPACC" % self.cmdn)
+            tdo_dec = parse_dpap_tdo(tdo, "cmd %u DPACC" % self.cmdn)
             return ir, tdi_dec, tdo_dec, self.cmdn
         elif ir == "APACC":
             # Table 3-7 JTAG-DP target response summary, when previous scan a was an APACC access
-            tdi_dec = parse_dpap_tdi(tdi, "APACC")
-            tdo_dec = parse_dpap_tdo(tdo, "APACC")
+            tdi_dec = parse_dpap_tdi(tdi, "cmd %u APACC" % self.cmdn)
+            tdo_dec = parse_dpap_tdo(tdo, "cmd %u APACC" % self.cmdn)
             return ir, tdi_dec, tdo_dec, self.cmdn
         else:
             assert 0, ir
 
     def run(self, fn_in):
+        # TODO: consider printing initial TDO
+        # Currently they are only in low level debug info
+
         self.cmds = self.iter_jtag_wr(fn_in)
         last_data = None
         done = False
@@ -353,12 +386,23 @@ class EyeTAG:
             try:
                 this_data = self.next_data()
             except StopIteration:
-                # There will be one orphaned last
-                # raise
+                # There will be one orphaned transaction
+                self.next_decode(last_data, None)
                 break
 
+            this_ir = this_data[0]
             if last_data:
-                self.next_decode(last_data, this_data)
+                last_ir = last_data[0]
+                # print("iter cmdn last %u, this %u" % (last_data[-1], this_data[-1]), this_ir, last_ir)
+                # when IR switches DR is essentially lost
+                # we'll get a response but its essentially a new transaction
+                if this_ir != last_ir:
+                    self.next_decode(last_data, None)
+                else:
+                    self.next_decode(last_data, this_data)
+            else:
+                # print("iter cmdn this %u" % this_data[-1])
+                pass
             last_data = this_data
 
 
